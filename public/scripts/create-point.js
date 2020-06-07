b35b769237b7b7064ec1b81a229cbcb6b74c08ef
Affine <pb.affine@gmail.com>
2020-06-07 14:36:33 -0300

    Primeiro commit do Projeto NLW - Starter

---
+++ public/scripts/create-point.js
@@ -0,0 +1,99 @@
+//Dados da Entidade
+
+function populateUfs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
+        for(const state of states) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    } )
+}
+
+populateUfs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true;
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        for(const city of cities) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false;
+    } )
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities) 
+
+//Itens de Coleta
+
+//Pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items")
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+
+    //Add or Remove [Adicionar ou remover uma classe com JavaScript]
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    //Verificar se existem itens selecionados, se sim pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex( item => item == itemId)
+
+    /*
+     Método tradicional da linha anterior 
+     const alreadySelected = selectedItems.findIndex( item => {
+         const itemFound = item == itemId //Isso será true ou false
+         return itemFound
+     }
+    */
+
+    //Se já estiver selecionado, tirar da seleção
+    if (alreadySelected >= 0) {
+        //Tirar da Seleção
+         const filteredItems = selectedItems.filter( item => item != itemId)
+        /*
+         Método tradicional da linha anterior
+         const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        */
+       selectedItems = filteredItems
+    }
+    //Se não estiver selecionado, adicionar à seleção
+    else{
+        selectedItems.push(itemId)
+    }
+
+    //Atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+    
+}
---
+++ public/scripts/intro.js
@@ -0,0 +1,58 @@
+// comments
+// document.write("Hello");
+
+// Variáveis, tipos de dados
+// let myvar = "Hello";
+// const myconst = "He";
+// document.write(myconst + myvar);
+
+// String 
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso é uma string'
+// const s3 = `Isso é string`
+// document.write(s2)
+
+// Number
+// const n1 = "1"
+// const n2 = 12
+// document.write(n1+n2)
+
+// Boolean
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// Objeto possuem propriedades e funcionalidades ...
+// const pessoa = { 
+//   altura: "1.75m",
+//   idade: 29,
+//   solteiro: false,
+//   correr() {
+//     document.write("Executar uma grande lógica de corrida")
+//   }
+// }
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de Código 
+// function sayMyName(name){
+//   document.write(name)
+// }
+// sayMyName("Gisele")
+// sayMyName("Thiago")
+
+// Condições
+// const notaFinal = 4
+// if (notaFinal > 5) {
+//   document.write("Aprovado")
+// } else {  
+//   document.write("Reprovado")
+// }
+
+// Loop / Repetições
+// for (i = 0 ; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
